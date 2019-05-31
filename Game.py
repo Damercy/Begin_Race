171ad270228756d96b0bf640b329fde1e44796f2
Dayamoy Adhikari <adhikaridayamoy@gmail.com>
2019-05-31 13:36:05 +0530

    Added buttons,buttons functionality,game_exit func.

--- Game.py
+++ Game.py
@@ -13,13 +13,17 @@ red = (255,0,0)
 green = (0,255,0)
 blue = (0,0,255)
 
+dim_red = (200,0,0)
+dim_white = (200,200,200)
+
 car_width = 111
 
 gameDisplay = pygame.display.set_mode((display_width ,display_height))
-pygame.display.set_caption('A bit racey part 2 lol!')
+pygame.display.set_caption('The GAME!')
 clock = pygame.time.Clock()
 
 carImg = pygame.image.load('car_mod.png')
+
 
 def things_dodged(count):
     font = pygame.font.SysFont(None,25)
@@ -36,9 +40,16 @@ def car(x,y):
     gameDisplay.blit(carImg,(x,y))
 
 def text_objects(text,font):
+    if text == 'START' or text == 'EXIT':
+        textSurface = font.render(text ,True,black)
+        return textSurface,textSurface.get_rect()
+
     textSurface = font.render(text ,True,white)
     return textSurface,textSurface.get_rect()
-    
+
+def game_exit():
+    pygame.quit()
+    quit()
 
     
 def message_display(text):
@@ -56,20 +67,43 @@ def message_display(text):
 def crash():
     message_display('Crashed!')
 
+def button(msg,x,y,w,h,color_active,color_inactive,action=None):
+    
+        mouse = pygame.mouse.get_pos()
+
+        click = pygame.mouse.get_pressed()
+        
+        pygame.draw.rect(gameDisplay,color_inactive,(x,y,w,h))
+        pygame.draw.rect(gameDisplay,color_inactive,(x,y,w,h))
+
+        if (x+w>mouse[0]>x) and (y+h>mouse[1]>y):
+            pygame.draw.rect(gameDisplay,color_active,(x,y,w,h))
+            if click[0] == 1 and action!=None:
+                action()
+        
+        smallText = pygame.font.Font("freesansbold.ttf",10)
+        TextSurf, TextRect = text_objects(msg,smallText)
+        TextRect.center = (((2*x+w)/2,(2*y+h)/2))
+        gameDisplay.blit(TextSurf,TextRect)
+
+
 def intro_screen():
     intro = True
     while intro:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()
+                game_exit()
         gameDisplay.fill(black)
         largeText = pygame.font.Font('freesansbold.ttf',60)
         TextSurf, TextRect = text_objects('Welcome to THE GAME!',largeText)
         TextRect.center = ((display_width/2),(display_height/2))
         gameDisplay.blit (TextSurf,TextRect)
+
+        button('START',150,450,100,50,dim_white,white,game_loop)
+        button('EXIT',550,450,100,50,dim_white,white,game_exit)
+            
         pygame.display.update()
-        clock.tick(2)
+        clock.tick(60)
 
     
 def game_loop():
@@ -120,7 +154,7 @@ def game_loop():
         thing_starty += thing_speed
         car(x,y)
         things_dodged(dodged)
-
+  
         if x > display_width - car_width or x< 0:
             crash()
         if thing_starty > display_height:
@@ -138,6 +172,6 @@ def game_loop():
             
         pygame.display.update()
         clock.tick(60)
+
 intro_screen()
-game_loop()
 
